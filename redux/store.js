92902f8dc0d001ef938cd85ba0eddde5a46cc481
malzak <malzovich@gmail.com>
2022-04-07 13:12:26 +0100

    commit

---
+++ redux/store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from "redux";
+import reducerCourses from "./reducers/reducerCourses";
+
+const rootReducer = combineReducers({
+  courses: reducerCourses,
+});
+const store = createStore(rootReducer);
+
+export default store;
